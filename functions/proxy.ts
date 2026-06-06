da51ac044c5eca0c9f06c3aa4bd6a3e2763ca20a
yhfhmn <163280001+yhfhmn@users.noreply.github.com>
2026-06-06 19:45:32 +0800

    Update proxy.ts

--- functions/proxy.ts
+++ functions/proxy.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://meting.elysium-stack.cn/api";
+const API_BASE_URL = "https://api.injahow.cn/music/";
 const KUWO_HOST_PATTERN = /(^|\.)kuwo\.cn$/i;
 const SAFE_RESPONSE_HEADERS = ["content-type", "cache-control", "accept-ranges", "content-length", "content-range", "etag", "last-modified", "expires"];
 
